0a96a86fad41da58f47a980675ddbd5b42819204
kalayciburak <kalayciburak1996@gmail.com>
2023-05-07 14:01:03 +0300

    add discovery-server

--- filter-service/src/main/java/com/kodlamaio/filterservice/FilterServiceApplication.java
+++ filter-service/src/main/java/com/kodlamaio/filterservice/FilterServiceApplication.java
@@ -2,7 +2,9 @@ package com.kodlamaio.filterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FilterServiceApplication {
 
---
+++ filter-service/src/main/java/com/kodlamaio/filterservice/business/dto/responses/GetFilterResponse.java
@@ -0,0 +1,25 @@
+package com.kodlamaio.filterservice.business.dto.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetFilterResponse {
+    private UUID id;
+    private UUID carId;
+    private UUID modelId;
+    private UUID brandId;
+    private String modelName;
+    private String brandName;
+    private String plate;
+    private int modelYear;
+    private double dailyPrice;
+    private String state;
+}
