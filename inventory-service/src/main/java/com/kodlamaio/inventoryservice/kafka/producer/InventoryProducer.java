f9ee96e3f24d6582b957bbb1b23bacc2e769f6d6
kalayciburak <kalayciburak1996@gmail.com>
2023-05-07 15:00:18 +0300

    07-05-23

---
+++ inventory-service/src/main/java/com/kodlamaio/inventoryservice/kafka/producer/InventoryProducer.java
@@ -0,0 +1,15 @@
+package com.kodlamaio.inventoryservice.kafka.producer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryProducer {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendMessage() {
+
+    }
+}
