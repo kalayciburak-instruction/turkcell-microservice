b1a5d4cd2bc1128e56f3e11300585dc149d98de1
kalayciburak <kalayciburak1996@gmail.com>
2023-05-26 01:50:15 +0300

    add missing services

--- rental-service/src/main/java/com/kodlamaio/rentalservice/business/rules/RentalBusinessRules.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/business/rules/RentalBusinessRules.java
@@ -1,27 +1,40 @@
 package com.kodlamaio.rentalservice.business.rules;
 
+import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
+import com.kodlamaio.commonpackage.utils.dto.CreateRentalPaymentRequest;
 import com.kodlamaio.commonpackage.utils.exceptions.BusinessException;
 import com.kodlamaio.rentalservice.api.clients.CarClient;
+import com.kodlamaio.rentalservice.api.clients.PaymentClient;
 import com.kodlamaio.rentalservice.repository.RentalRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class RentalBusinessRules {
     private final RentalRepository repository;
-    private final CarClient client;
+    private final CarClient carClient;
+    private final PaymentClient paymentClient;
 
     public void checkIfRentalExists(UUID id) {
         if (!repository.existsById(id)) {
-            throw new BusinessException("MODEL_NOT_EXISTS");
+            throw new BusinessException("RENTAL_NOT_EXISTS");
         }
     }
 
     public void ensureCarIsAvailable(UUID carId) {
-        var response = client.checkIfCarAvailable(carId);
+        var response = carClient.checkIfCarAvailable(carId);
+        checkClientResponse(response);
+    }
+
+    public void ensurePayment(CreateRentalPaymentRequest request) {
+        var response = paymentClient.processPayment(request);
+        checkClientResponse(response);
+    }
+
+    private void checkClientResponse(ClientResponse response) {
         if (!response.isSuccess()) {
             throw new BusinessException(response.getMessage());
         }
